36cf744a347e999fc82cf9cccd8890878e3c821c
Thierno Diallo <tdiallo1996@gmail.com>
2024-11-29 22:38:03 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Thierno Diallo on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
